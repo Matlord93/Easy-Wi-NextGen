c924dfdad5889fab1f29a840f7dc41d3fa697084
Matlord93 <99674650+Matlord93@users.noreply.github.com>
2026-06-23 06:04:13 +0200

    Fix class declaration syntax in MusicbotTeamspeakBackendConfigRepository

--- core/src/Repository/MusicbotTeamspeakBackendConfigRepository.php
+++ core/src/Repository/MusicbotTeamspeakBackendConfigRepository.php
@@ -10,7 +10,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /** @extends ServiceEntityRepository<MusicbotTeamspeakBackendConfig> */
-final class MusicbotTeamspeakBackendConfigRepository extends ServiceEntityRepository
+class MusicbotTeamspeakBackendConfigRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
